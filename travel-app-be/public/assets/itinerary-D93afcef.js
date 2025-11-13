b7634c66933e112d8a8adaab63634bf1a9264394
Vamsi Kolli <vamsi.kolli10@gmail.com>
2025-11-13 16:36:38 -0500

    hosting fixes

---
+++ travel-app-be/public/assets/itinerary-D93afcef.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-Tw1uk1st.js";async function n(a={}){const{data:t}=await e.get("/itinerary/generate",{params:a});return t}export{n as generateItinerary};
---
+++ travel-app-be/public/assets/itinerary-YZAId6nC.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-Clv_JTco.js";async function n(a={}){const{data:t}=await e.get("/itinerary/generate",{params:a});return t}export{n as generateItinerary};
